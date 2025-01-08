e2b065dd66cf2e92721b99170542e3fa867370db
MUFArd <muhammadfarelandriani@gmail.com>
2025-01-08 21:35:32 +0700

    update

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon web" type="image/png" href="./img/Logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>News-Birama</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <h1>News-Birama</h1>
+        </div>
+        <nav>
+            <ul class="main-links">
+                <li><a href="index.html">Beranda</a></li>
+                <li><a href="">Tentang Kami</a></li>
+                <li>
+                    <a href="">Kegiatan Sekolah</a>
+                    <i class="fa-solid fa-chevron-down"></i>
+                    <ul class="sub-links">
+                        <li><a href="">Tadarus Pagi</a></li>
+                        <li><a href="">Literasi</a></li>
+                        <li><a href="">Sholat dhuha bersama</a></li>
+                    </ul>
+                </li>
+                <li><a href="">Profil Sekolah</a></li>
+                <li><a href="">Staf & Tata Usaha</a></li>
+            </ul>
+            <div class="search">
+                <i class="fa-solid fa-search"></i>
+            </div>
+            <div class="profil">
+                <i class="fa-solid fa-user"></i>
+            </div>
+
+            <div class="tanggal">
+                <h2 id="jam"></h2>
+                <p id="tanggal"></p>
+            </div>
+        </nav>
+        <div class="tag-links">
+            <ul>
+                <li><a href="">MPK</a></li>
+                <li><a href="">Osis</a></li>
+                <li><a href="">Jurnalistik</a></li>
+                <li><a href="">Pramuka</a></li>
+                <li><a href="">PMR</a></li>
+                <li><a href="">Natbinari</a></li>
+                <li><a href="">Paskibra</a></li>
+                <li><a href="">Marcingband</a></li>
+                <li><a href="">Voli</a></li>
+                <li><a href="">Badminton</a></li>
+                <li><a href="">Futsal</a></li>
+                <li><a href="">Silat</a></li>
+                <li><a href="">Hadroh</a></li>
+                <li><a href="">Tari</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        <div class="Top-News">
+            <h1>Heading 1</h1>
+            <img src="" alt="" srcset="">
+    </div>
+    </main>
+</body>
+<script src="script.js"></script>
+</html> 
---
+++ script.js
@@ -0,0 +1,23 @@
+function updateTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours().toString().padStart(2, '0');
+    var minutes = currentTime.getMinutes().toString().padStart(2, '0');
+    var seconds = currentTime.getSeconds().toString().padStart(2, '0');
+    var timeString = hours + ':' + minutes + ':' + seconds;
+
+    var daysOfWeek = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+    var dayOfWeek = daysOfWeek[currentTime.getDay()];
+    var day = currentTime.getDate().toString().padStart(2, '0');
+    var month = (currentTime.getMonth() + 1).toString().padStart(2, '0');
+    var year = currentTime.getFullYear();
+
+    var dateString = dayOfWeek + ', ' + day + '/' + month + '/' + year;
+
+    document.getElementById('jam').textContent = timeString;
+    document.getElementById('tanggal').textContent = dateString;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+    
